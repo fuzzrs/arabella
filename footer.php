8a15c0722eec33667ff7000246f007c66803d1f5
Leonardo Santos <fuzzrs@gmail.com>
2018-07-12 17:07:37 -0300

    nova pag e mudanca de html para php

---
+++ footer.php
@@ -0,0 +1,80 @@
+	<footer class="footer">
+		<div class="wrapper">
+			<div class="flex-align">
+				<div class="footer-column">
+					<h3>Contato</h3>
+					<p>Arabella Couros do Sul</p>
+					<ul>
+						<li><a href="tel:5432865099">(54) 3286-5099</a> - <a href="tel:54999042429">(54) 99904-2429</a></li>
+						<li><a href="mailto:contato@arabella.com.br">contato@arabella.com.br</a></li>
+						<li>Av. Bela Vista, 37, Centro, Gramado/RS</li>
+					</ul>
+					
+					<!-- <p>Lojas físicas</p>
+					<ul>
+						<li>Gramado - Matriz</li>
+						<li>Gramado - Galeria</li>
+						<li>Canela</li>
+						<li>Nova Petrópolis</li>
+					</ul> -->
+				</div>
+				<div class="footer-column">
+					<h3>Lojas Físicas</h3>
+					<ul>
+						<li><a href="#">Gramado - Matriz</a></li>
+						<li><a href="#">Gramado - Galeria</a></li>
+						<li><a href="#">Canela</a></li>
+						<li><a href="#">Nova Petrópolis</a></li>
+					</ul>
+				</div>
+				<div class="footer-column">
+					<h3>Utilidades</h3>
+					<ul>
+						<li><a href="#">criar conta</a></li>
+						<li><a href="#">acessar conta</a></li>
+						<li><a href="#">Perguntas frequentes</a></li>
+						<li><a href="#" class="whatsapp-btn">
+							<img src="images/icons/whatsapp.svg" alt="">
+							<span>Contato via Whatsapp!</span>
+						</a></li>
+					</ul>
+				</div>
+				<div class="footer-column">
+					<h3>Siga a gente</h3>
+					<ul>
+						
+						<li><a href="https://facebook.com/">Facebook</a></li>
+						<li><a href="https://instagram.com/">Instagram</a></li>
+						<li><a href="https://instagram.com/">Youtube</a></li>
+						<li><a href="#" class="fb-btn">
+							<img src="images/icons/fb-like.svg" width="16px" alt="">
+							<p><span>Curtir</span> 1,2 mi</p>
+						</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<div class="copyright">
+		<div class="wrapper">
+			Arabella © 2018 - todos os direitos reservados - 
+			Arabella Couros ME / CNPJ: 08.747.622/0001-90 / Inscrição Estadual: 0560094639
+		</div>
+	</div>
+
+	<script>
+		// $('.review-slide').slick();
+
+		$(document).ready(function(){
+			$('.review-slide').slick({
+				infinite: true,
+				autoplay: true,
+				slidesToShow: 1,
+				arrows: true,
+				prevArrow: "prev",
+				nextArrow: 'next'
+			});
+		});
+	</script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,279 @@
+<?php include 'header.php'; ?>
+
+
+	<main class="main-content">
+
+		<div class="content-section" style="padding-top: 0; margin-bottom: 0;">
+			<div class="wrapper">
+				<h2 class="section-title">Ofertas da semana</h2>
+				<div class="product-grid">
+					<div class="product-item">
+						<a href="#" class="product-fav"></a>
+						<a href="#" class="product-description-url">
+							<div class="product-item-image"><img src="images/jpg/tapete-mandala-2.jpg" alt=""></div>
+							<div class="product-item-name">Tapete de Couro de boi mandala</div>
+							<div class="product-item-price">R$ 475,00</div>
+						</a>
+						<div class="product-item-buttons">
+							<a href="#" class="link">Detalhes</a><a href="#" class="btn">Comprar</a>
+						</div>
+					</div>
+					<div class="product-item">
+						<a href="#" class="product-fav"></a>
+						<a href="#" class="product-description-url">
+							<div class="product-item-image"><img src="images/jpg/branco-2-1.jpg" alt=""></div>
+							<div class="product-item-name">Tapete De Couro Arabella Quadriculado Branco 1,5x2m Placa 10cm</div>
+							<div class="product-item-price">
+								<span class="original-price">R$ 1.008,00</span>
+								<span class="promo-price">R$ 484,00</span>
+							</div>
+						</a>
+						<div class="product-item-buttons">
+							<a href="#" class="link">Detalhes</a><a href="#" class="btn">Comprar</a>
+						</div>
+					</div>
+					<div class="product-item">
+						<a href="#" class="product-fav"></a>
+						<a href="#" class="product-description-url">
+							<div class="product-item-image"><img src="images/jpg/inteiro-malhado-preto.jpg" alt=""></div>
+							<div class="product-item-name">Tapete De Couro De Boi Inteiro Malhado Preto E Branco</div>
+							<div class="product-item-price">
+								<span class="original-price">R$ 889,00</span>
+								<span class="promo-price">R$ 627,00</span>
+							</div>
+						</a>
+						<div class="product-item-buttons">
+							<a href="#" class="link">Detalhes</a><a href="#" class="btn">Comprar</a>
+						</div>
+					</div>
+					<div class="product-item">
+						<a href="#" class="product-fav"></a>
+						<a href="#" class="product-description-url">
+							<div class="product-item-image"><img src="images/jpg/tapetes-boi/tapete-de-couro-de-boi-cores.jpg" alt=""></div>
+							<div class="product-item-name">Tapete De Couro De Boi Caramelo E Branco</div>
+							<div class="product-item-price">R$ 275,00</div>
+						</a>
+						<div class="product-item-buttons">
+							<a href="#" class="link">Detalhes</a><a href="#" class="btn">Comprar</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+		<div class="content-section categories-section">
+			<div class="wrapper">
+				<h2 class="section-title">Descubra produtos incríveis em nossas categorias!</h2>
+			</div>
+			<div class="wrapper">
+				<div class="flex-align">
+					<a href="#" class="category-box"><p>vestuário</p></a>
+					<a href="#" class="category-box"><p>tapetes de couro</p></a>
+					<a href="#" class="category-box"><p>tapetes de lã</p></a>
+					<a href="#" class="category-box"><p>decoração</p></a>
+					<a href="#" class="category-box"><p>peles exóticas</p></a>
+				</div>
+			</div>
+		</div>
+
+		<div class="banner-section">
+			<div class="banner-content">
+				
+			</div>
+		</div>
+
+		<div class="content-section">
+			<div class="wrapper">
+				<h2 class="section-title">Tapetes</h2>
+				<div class="product-grid" style="margin-bottom: 80px">
+					<div class="product-item">
+						<a href="#" class="product-fav"></a>
+						<a href="#" class="product-description-url">
+							<div class="product-item-image"><img src="images/jpg/tapetes-boi/tapete-especial-catavento.jpg" alt=""></div>
+							<div class="product-item-name">Tapete de couro de boi especial catavento branco e preto 2×2,5m</div>
+							<div class="product-item-price">R$ 1.835,00</div>
+						</a>
+						<div class="product-item-buttons">
+							<a href="#" class="link">Detalhes</a><a href="#" class="btn">Comprar</a>
+						</div>
+					</div>
+					<div class="product-item">
+						<a href="#" class="product-fav"></a>
+						<a href="#" class="product-description-url">
+							<div class="product-item-image"><img src="images/jpg/tapetes-boi/tapete-mandala-marrom.jpg" alt=""></div>
+							<div class="product-item-name">Tapete de couro de boi redondo mandala branco e marrom</div>
+							<div class="product-item-price">R$ 675,00</div>
+						</a>
+						<div class="product-item-buttons">
+							<a href="#" class="link">Detalhes</a><a href="#" class="btn">Comprar</a>
+						</div>
+					</div>
+					<div class="product-item">
+						<a href="#" class="product-fav"></a>
+						<a href="#" class="product-description-url">
+							<div class="product-item-image"><img src="images/jpg/tapetes-boi/tapete-piramide-preto-caramelo-cinza.jpg" alt=""></div>
+							<div class="product-item-name">Tapete De Couro De Boi Especial Piramide Preto, Cinza E Caramelo 2×2,5m</div>
+							<div class="product-item-price">R$ 1.285,00</div>
+						</a>
+						<div class="product-item-buttons">
+							<a href="#" class="link">Detalhes</a><a href="#" class="btn">Comprar</a>
+						</div>
+					</div>
+					<div class="product-item">
+						<a href="#" class="product-fav"></a>
+						<a href="#" class="product-description-url">
+							<div class="product-item-image"><img src="images/jpg/tapetes-boi/tapete-preto-branco.jpg" alt=""></div>
+							<div class="product-item-name">Tapete De Couro De Boi Preto E Branco</div>
+							<div class="product-item-price">R$ 275,00</div>
+						</a>
+						<div class="product-item-buttons">
+							<a href="#" class="link">Detalhes</a><a href="#" class="btn">Comprar</a>
+						</div>
+					</div>
+				</div>
+
+				<!-- <h2 class="section-title">Pelegos</h2> -->
+				<h2 class="section-title">Uma imensa variedade de produtos para você!</h2>
+				<div class="product-grid">
+					<div class="product-item">
+						<a href="#" class="product-fav"></a>
+						<a href="#" class="product-description-url">
+							<div class="product-item-image"><img src="images/jpg/produtos/pelego.jpg" alt=""></div>
+							<div class="product-item-name">Pelego de ovelha tom natural</div>
+							<div class="product-item-price">R$ 190,00</div>
+						</a>
+						<div class="product-item-buttons">
+							<a href="#" class="link">Detalhes</a><a href="#" class="btn">Comprar</a>
+						</div>
+					</div>
+					<div class="product-item">
+						<a href="#" class="product-fav"></a>
+						<a href="#" class="product-description-url">
+							<div class="product-item-image"><img src="images/jpg/produtos/jaqueta-masculina-pelica.jpg" alt=""></div>
+							<div class="product-item-name">Jaqueta de couro masculina pelica</div>
+							<div class="product-item-price">R$ 737,00</div>
+						</a>
+						<div class="product-item-buttons">
+							<a href="#" class="link">Detalhes</a><a href="#" class="btn">Comprar</a>
+						</div>
+					</div>
+					<div class="product-item">
+						<a href="#" class="product-fav"></a>
+						<a href="#" class="product-description-url">
+							<div class="product-item-image"><img src="images/jpg/produtos/almofadas-pele-coelho.jpg" alt=""></div>
+							<div class="product-item-name">Almofadas de pele de coelho</div>
+							<div class="product-item-price">R$ 220,00</div>
+						</a>
+						<div class="product-item-buttons">
+							<a href="#" class="link">Detalhes</a><a href="#" class="btn">Comprar</a>
+						</div>
+					</div>
+					<div class="product-item">
+						<a href="#" class="product-fav"></a>
+						<a href="#" class="product-description-url">
+							<div class="product-item-image"><img src="images/jpg/produtos/puff-couro-preto.jpg" alt=""></div>
+							<div class="product-item-name">Puff de couro preto</div>
+							<div class="product-item-price">
+								<span class="original-price">R$ 350,00</span>
+								<span class="promo-price">R$ 220,00</span>
+							</div>
+						</a>
+						<div class="product-item-buttons">
+							<a href="#" class="link">Detalhes</a><a href="#" class="btn">Comprar</a>
+						</div>
+					</div>
+				</div>
+				
+				<p style="padding: 120px 0 32px; text-align: center; font-size: 32px">
+					Aproveite as melhores ofertas! <a href="#">Ver Loja</a>
+				</p>
+			</div>
+		</div>
+
+		<div class="content-section review-section">
+			<div class="wrapper">
+				<div class="review-slide">
+					<div class="review-item">
+						<div class="flex-align">
+							<div class="review-photo">
+								<img src="images/jpg/depoimentos/amelia-cunha.jpg" alt="">
+							</div>
+							<div class="review-text">
+								<p>
+									Chegou o tapete agora, perfeito, lindo e muito bom... Vou indicar à medida que minhas
+									amigas gostarem...
+								</p>
+								<span>Amelia Cunha<br/>Belo Horizonte/MG</span>
+							</div>
+						</div>
+					</div>
+					<div class="review-item">
+						<div class="flex-align">
+							<div class="review-photo"></div>
+							<div class="review-text">
+								<p>
+									Chegou o tapete agora, perfeito, lindo e muito bom... Vou indicar à medida que minhas
+									amigas gostarem...
+								</p>
+								<span>Amelia Cunha - Belo Horizonte/MG</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="content-section">
+			<div class="wrapper">
+				<h2 class="section-title">Você pode se interessar</h2>
+				<div class="flex-align">
+					<div class="blog-item">
+						<div class="image image-1"></div>
+						<div class="blog-text">
+							<h3 class="title">Tendências 2018 sobreposição de tapetes</h3>
+							<p class="text">
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis suscipit, sapien eu posuere consectetur, tellus magna malesuada felis...
+							</p>
+							<a href="#" class="btn" style="margin-top: 24px">Leia mais</a>
+						</div>
+					</div>
+					<div class="blog-item">
+						<div class="image image-2"></div>
+						<div class="blog-text">
+							<h3 class="title">Dicas de tapete para cada cômodo da casa</h3>
+							<p class="text">
+								Uma maneira fácil e rápida de decorar ou redecorar o interior de sua casa é apostando nos tapetes. Sendo eles uma peça coringa...
+							</p>
+							<a href="#" class="btn" style="margin-top: 24px">Leia mais</a>
+						</div>
+					</div>
+					<div class="blog-item">
+						<div class="image image-3"></div>
+						<div class="blog-text">
+							<h3 class="title">Como usar pelêgos na decoração de sua casa</h3>
+							<p class="text">
+								Pelegos são peles de carneiro ou ovelha, são um item de decoração que podem ser usados de diversas formas e em diversos locais...
+							</p>
+							<a href="#" class="btn" style="margin-top: 24px">Leia mais</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="content-section newsletter-section">
+			<div class="wrapper">
+				<div class="newsletter-content">
+					<h2 class="section-title">Receba nossas novidades!</h2>
+					<form class="newsletter-form" action="">
+						<input class="newsletter-input" type="text" placeholder="Digite seu e-mail principal">
+						<input class="newsletter-button" type="submit" value="Assinar">
+					</form>
+				</div>
+			</div>
+		</div>
+
+	</main>
+
+<?php include 'footer.php'; ?>
