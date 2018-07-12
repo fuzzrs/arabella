c48cd85d369148edc8731beade530b8ae45ac812
Leonardo Santos <fuzzrs@gmail.com>
2018-07-12 17:26:59 -0300

    new commit

---
+++ Nova pasta (2)/lista-produtos.php
@@ -0,0 +1,164 @@
+<?php include 'header.php'; ?>
+
+
+<main class="main-content product-list-page">
+	<div class="wrapper">
+		<aside class="sidebar">
+			<h3>Categorias</h3>
+			
+			<ul>
+				<li><a href="#">Tapetes de couro</a></li>
+				<li><a href="#">Tapetes de lã</a></li>
+				<li><a href="#">Decor</a></li>
+			</ul>
+		</aside>
+		<div class="page-content">
+			<div class="product-list">
+				<div class="product-item">
+						<a href="#" class="product-fav"></a>
+						<a href="#" class="product-description-url">
+							<div class="product-item-image"><img src="images/jpg/tapete-mandala-2.jpg" alt=""></div>
+							<div class="product-item-name">Tapete de Couro de boi mandala</div>
+							<div class="product-item-price">R$ 475,00</div>
+						</a>
+						<div class="product-item-buttons">
+							<a href="#" class="link">Detalhes</a><a href="#" class="btn">Comprar</a>
+						</div>
+					</div>
+					<div class="product-item">
+						<a href="#" class="product-fav"></a>
+						<a href="#" class="product-description-url">
+							<div class="product-item-image"><img src="images/jpg/branco-2-1.jpg" alt=""></div>
+							<div class="product-item-name">Tapete De Couro Arabella Quadriculado Branco 1,5x2m Placa 10cm</div>
+							<div class="product-item-price">
+								<span class="original-price">R$ 1.008,00</span>
+								<span class="promo-price">R$ 484,00</span>
+							</div>
+						</a>
+						<div class="product-item-buttons">
+							<a href="#" class="link">Detalhes</a><a href="#" class="btn">Comprar</a>
+						</div>
+					</div>
+					<div class="product-item">
+						<a href="#" class="product-fav"></a>
+						<a href="#" class="product-description-url">
+							<div class="product-item-image"><img src="images/jpg/inteiro-malhado-preto.jpg" alt=""></div>
+							<div class="product-item-name">Tapete De Couro De Boi Inteiro Malhado Preto E Branco</div>
+							<div class="product-item-price">
+								<span class="original-price">R$ 889,00</span>
+								<span class="promo-price">R$ 627,00</span>
+							</div>
+						</a>
+						<div class="product-item-buttons">
+							<a href="#" class="link">Detalhes</a><a href="#" class="btn">Comprar</a>
+						</div>
+					</div>
+					<div class="product-item">
+						<a href="#" class="product-fav"></a>
+						<a href="#" class="product-description-url">
+							<div class="product-item-image"><img src="images/jpg/tapetes-boi/tapete-de-couro-de-boi-cores.jpg" alt=""></div>
+							<div class="product-item-name">Tapete De Couro De Boi Caramelo E Branco</div>
+							<div class="product-item-price">R$ 275,00</div>
+						</a>
+						<div class="product-item-buttons">
+							<a href="#" class="link">Detalhes</a><a href="#" class="btn">Comprar</a>
+						</div>
+					</div>
+					<div class="product-item">
+						<a href="#" class="product-fav"></a>
+						<a href="#" class="product-description-url">
+							<div class="product-item-image"><img src="images/jpg/tapetes-boi/tapete-especial-catavento.jpg" alt=""></div>
+							<div class="product-item-name">Tapete de couro de boi especial catavento branco e preto 2×2,5m</div>
+							<div class="product-item-price">R$ 1.835,00</div>
+						</a>
+						<div class="product-item-buttons">
+							<a href="#" class="link">Detalhes</a><a href="#" class="btn">Comprar</a>
+						</div>
+					</div>
+					<div class="product-item">
+						<a href="#" class="product-fav"></a>
+						<a href="#" class="product-description-url">
+							<div class="product-item-image"><img src="images/jpg/tapetes-boi/tapete-mandala-marrom.jpg" alt=""></div>
+							<div class="product-item-name">Tapete de couro de boi redondo mandala branco e marrom</div>
+							<div class="product-item-price">R$ 675,00</div>
+						</a>
+						<div class="product-item-buttons">
+							<a href="#" class="link">Detalhes</a><a href="#" class="btn">Comprar</a>
+						</div>
+					</div>
+					<div class="product-item">
+						<a href="#" class="product-fav"></a>
+						<a href="#" class="product-description-url">
+							<div class="product-item-image"><img src="images/jpg/tapetes-boi/tapete-piramide-preto-caramelo-cinza.jpg" alt=""></div>
+							<div class="product-item-name">Tapete De Couro De Boi Especial Piramide Preto, Cinza E Caramelo 2×2,5m</div>
+							<div class="product-item-price">R$ 1.285,00</div>
+						</a>
+						<div class="product-item-buttons">
+							<a href="#" class="link">Detalhes</a><a href="#" class="btn">Comprar</a>
+						</div>
+					</div>
+					<div class="product-item">
+						<a href="#" class="product-fav"></a>
+						<a href="#" class="product-description-url">
+							<div class="product-item-image"><img src="images/jpg/tapetes-boi/tapete-preto-branco.jpg" alt=""></div>
+							<div class="product-item-name">Tapete De Couro De Boi Preto E Branco</div>
+							<div class="product-item-price">R$ 275,00</div>
+						</a>
+						<div class="product-item-buttons">
+							<a href="#" class="link">Detalhes</a><a href="#" class="btn">Comprar</a>
+						</div>
+					</div>
+					<div class="product-item">
+						<a href="#" class="product-fav"></a>
+						<a href="#" class="product-description-url">
+							<div class="product-item-image"><img src="images/jpg/produtos/pelego.jpg" alt=""></div>
+							<div class="product-item-name">Pelego de ovelha tom natural</div>
+							<div class="product-item-price">R$ 190,00</div>
+						</a>
+						<div class="product-item-buttons">
+							<a href="#" class="link">Detalhes</a><a href="#" class="btn">Comprar</a>
+						</div>
+					</div>
+					<div class="product-item">
+						<a href="#" class="product-fav"></a>
+						<a href="#" class="product-description-url">
+							<div class="product-item-image"><img src="images/jpg/produtos/jaqueta-masculina-pelica.jpg" alt=""></div>
+							<div class="product-item-name">Jaqueta de couro masculina pelica</div>
+							<div class="product-item-price">R$ 737,00</div>
+						</a>
+						<div class="product-item-buttons">
+							<a href="#" class="link">Detalhes</a><a href="#" class="btn">Comprar</a>
+						</div>
+					</div>
+					<div class="product-item">
+						<a href="#" class="product-fav"></a>
+						<a href="#" class="product-description-url">
+							<div class="product-item-image"><img src="images/jpg/produtos/almofadas-pele-coelho.jpg" alt=""></div>
+							<div class="product-item-name">Almofadas de pele de coelho</div>
+							<div class="product-item-price">R$ 220,00</div>
+						</a>
+						<div class="product-item-buttons">
+							<a href="#" class="link">Detalhes</a><a href="#" class="btn">Comprar</a>
+						</div>
+					</div>
+					<div class="product-item">
+						<a href="#" class="product-fav"></a>
+						<a href="#" class="product-description-url">
+							<div class="product-item-image"><img src="images/jpg/produtos/puff-couro-preto.jpg" alt=""></div>
+							<div class="product-item-name">Puff de couro preto</div>
+							<div class="product-item-price">
+								<span class="original-price">R$ 350,00</span>
+								<span class="promo-price">R$ 220,00</span>
+							</div>
+						</a>
+						<div class="product-item-buttons">
+							<a href="#" class="link">Detalhes</a><a href="#" class="btn">Comprar</a>
+						</div>
+					</div>
+			</div>
+		</div>
+	</div>
+</main>
+
+
+<?php include 'footer.php'; ?>
